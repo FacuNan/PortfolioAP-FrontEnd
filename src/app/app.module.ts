2624b110acd3812a3ae89e08376803c5f59e69c5
Facundo Comercio <facundocomercio@outlook.com>
2022-12-02 19:32:17 -0300

    arquitectura portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { AcercaDeMiComponent } from './componentes/acerca-de-mi/acerca-de-mi.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    PerfilComponent,
+    AcercaDeMiComponent,
+    EducacionComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
