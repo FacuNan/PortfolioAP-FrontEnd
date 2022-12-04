5abcd7dd7fcd9e9918102d9d740793a8b3a4f442
Facundo Comercio <facundocomercio@outlook.com>
2022-12-04 13:06:15 -0300

    probando sintaxis de JWT en angular

--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -1,10 +1,24 @@
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Token } from '@angular/compiler';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  url=''
+  url ='http//localhost/3000/api';
+  token:any;
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  login(email: string, password: string){
+
+    this.http.post(this.url + 'authenticate', {email: email, password: password}).subscribe((resp: any)=>{
+      this.router.navigate(['profile']);
+
+      localStorage.setItem('auth_token', resp.token);
+    })
+
+  }
 }
