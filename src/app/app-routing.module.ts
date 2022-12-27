ef55df9a42185b398e6a6324dbdaa85599efa4f7
Facundo Comercio <facundocomercio@outlook.com>
2022-12-26 23:57:43 -0300

    routeo de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './componentes/formularios/login/login.component';
+import { HomeComponent } from './componentes/home/home.component';
+
+
 
 const routes: Routes = [
-  
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+
+  }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,10 @@ import { EducacionComponent } from './componentes/educacion/educacion.component'
 import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './componentes/login/login.component';
 import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 import { ContactosComponent } from './componentes/contactos/contactos.component';
 import { FooterComponent } from './componentes/footer/footer.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 @NgModule({
   declarations: [
@@ -23,10 +23,11 @@ import { FooterComponent } from './componentes/footer/footer.component';
     AcercaDeMiComponent,
     EducacionComponent,
     ExperienciaComponent,
-    LoginComponent,
+  
     ProyectosComponent,
     ContactosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
