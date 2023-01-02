b2c972e1f4873d309662800907d1bef51b1e46df
Facundo Comercio <facundocomercio@outlook.com>
2023-01-02 20:22:50 -0300

    validacion de formulario de contactos

--- src/app/componentes/contactos/contactos.component.ts
+++ src/app/componentes/contactos/contactos.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contactos',
   templateUrl: './contactos.component.html',
   styleUrls: ['./contactos.component.css']
 })
-export class ContactosComponent {
+export class ContactosComponent implements OnInit {
 
+  contactosForm: FormGroup = new FormGroup({});
+ constructor(private formBuilder: FormBuilder){}
+  ngOnInit(): void {
+    this.contactosForm = this.formBuilder.group({
+      nombre:['', Validators.compose([Validators.required, Validators.max(50)])],
+      email:['', [Validators.required, Validators.email]],
+      asunto:['', Validators.compose([Validators.required, Validators.maxLength(50)])],
+      mensaje:['', Validators.compose([Validators.required, Validators.max(200)])]
+    })
+  }
+
+  get nombre(){
+    return this.contactosForm.get('nombre')
+  }
+
+  get email(){
+    return this.contactosForm.get('email')
+  }
+
+  get asunto(){
+    return this.contactosForm.get('asunto')
+  }
+
+  get mensaje(){
+    return this.contactosForm.get('mensaje')
+  }
 }
