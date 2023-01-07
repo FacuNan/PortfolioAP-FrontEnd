c17a4f5f01d24834f82817583e864d754ce923cc
Facundo Comercio <facundocomercio@outlook.com>
2023-01-06 23:40:03 -0300

    sistema de routeos y aplicacion de Angular Material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcercaDeMiComponent } from './componentes/acerca-de-mi/acerca-de-mi.component';
+import { ContactosComponent } from './componentes/contactos/contactos.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 import { LoginComponent } from './componentes/formularios/login/login.component';
 import { HomeComponent } from './componentes/home/home.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
 
 
@@ -9,11 +16,44 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'home'
+    redirectTo: 'home/perfil'
   },
   {
     path: 'home',
-    component: HomeComponent
+    component: HomeComponent,
+    children: [
+      {
+        path: 'perfil',
+        component: PerfilComponent
+      },
+      {
+        path: 'biografia',
+        component: AcercaDeMiComponent
+      },
+      {
+        path: 'educacion',
+        component: EducacionComponent
+      },
+      {
+        path: 'experiencia',
+        component: ExperienciaComponent
+      },
+      {
+        path: 'proyectos',
+        component: ProyectosComponent
+      },
+      {
+        path: 'contacto',
+        component: ContactosComponent
+      },
+
+      {
+        path: 'footer',
+        component: FooterComponent
+      }
+
+    ]
+
   },
   {
     path: 'login',
