06fe67a65b17be71a6a028c64bcd441d0fbf5c70
Facundo Comercio <facundocomercio@outlook.com>
2023-02-12 12:58:56 -0300

    creacion del servicio

--- src/app/model/PersonaModel.ts
+++ src/app/model/PersonaModel.ts
@@ -4,4 +4,10 @@ export class Persona{
     apellido: String;
     img:String;
 
+    constructor(nombre:String, apellido: String, img: String){
+        this.nombre=nombre;
+        this.apellido = apellido;
+        this.img = img;
+    }
+
 }
