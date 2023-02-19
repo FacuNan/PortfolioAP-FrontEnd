7935730459138090e154b2715ddf2ad5211b7961
Facundo Comercio <facundocomercio@outlook.com>
2023-02-19 20:15:04 -0300

    creacion de servicios y entidades

--- src/app/componentes/proyectos/proyectos.component.ts
+++ src/app/componentes/proyectos/proyectos.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Proyectos } from 'src/app/model/proyectos';
+import { ProyectosServiceService } from 'src/app/servicios/proyectos-service.service';
+import { TokenService } from 'src/app/servicios/token.service';
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
-export class ProyectosComponent {
+export class ProyectosComponent implements OnInit{
+
+  proyectos: Proyectos[]=[];
+  isLogged = false;
+
+constructor(private sProyectos: ProyectosServiceService, private tokenService: TokenService){}
+
+
+  ngOnInit(): void {
+
+    this.cargarProyectos(); 
+    if(this.tokenService.getToken()){
+      this.isLogged=true;
+    }else{
+      this.isLogged=false;
+
+    }
+
+    
+  }
+
+  cargarProyectos():void{
+    this.sProyectos.lista().subscribe(data=>{
+      this.proyectos=data
+    })
+  }
+
+
 
 }
